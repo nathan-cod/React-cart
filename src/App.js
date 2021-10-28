465fe96d12cd18088276ebdf78ebdd25eb7bb81d
Nathan <nathantedla@gmail.com>
2021-10-28 02:14:43 -0700

    first phase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./components/Main";
+import Basket from "./components/Basket";
+import Header from "./components/Header";
+import data from "./data";
+import { useState } from "react";
+import Product from "./components/product";
 
 function App() {
+  const { product } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (Product) => {
+    const exist = cartItems.find((x) => x.id === products.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === products.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...Product, qty: 1 }]);
+    }
+  };
+  const onRemove = (Product) => {
+    const exist = cartItems.find((x) => x.id === Product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Main products={products} onAdd={onAdd}></Main>
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Basket>
+      </div>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Product from "./product";
+import data from "../data";
+export default function Main(props) {
+  const { products, onAdd } = props;
+  return (
+    <main className="block col-2">
+      <h2>Products</h2>
+      <div className="row">
+        {data.product.map((product) => (
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
+        ))}
+      </div>
+    </main>
+  );
+}
